1fc111e78b9b612b997a9f4e34229cb886108a8f
BUAYA <pic_gth@clickgroup.id>
2026-07-31 20:52:25 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,21 +1,28 @@
-(function () {
-  const css = `
-.site-header{
-    background:url("http://plcl.me/images/ysWgh.jpg") center/cover no-repeat!important;
-    position:relative!important;
-    z-index:50!important;
-}
+(function() {
+    const css = `
+      
+        .site-header {
+            background-image: url("https://lh3.googleusercontent.com/d/18lZ8xpyb-L9-Qd8Iiznj26Pp-Jtzo2Cp") !important;
+            background-size: cover !important;
+            background-position: center center !important;
+            background-repeat: no-repeat !important;
+            background-attachment: scroll !important;
+            position: relative !important;
+            z-index: 50 !important;
+        }
 
-@media (max-width:768px){
-    .site-header{
-        background-image:url("http://plcl.me/images/qjb7U.jpeg")!important;
-        background-position:left center!important;
-    }
-}
-`;
+        
+        @media (max-width: 768px) {
+            .site-header {
+                background-image: url("https://lh3.googleusercontent.com/d/1o6RU59Zbw7ajGxxW1GyhXFhzf5oXuOgx") !important;
+                background-size: cover !important;
+                background-position: left center !important; 
+                background-repeat: no-repeat !important;
+            }
+        }
+    `;
 
-  const style = document.createElement("style");
-  style.id = "gadunslot-header-bg";
-  style.textContent = css;
-  document.head.appendChild(style);
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = css;
+    document.head.appendChild(styleTag);
 })();
